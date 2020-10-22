e6b1efc2dc389635eeafcfcc910a2149b82a9cb9
rohit <kumbharrohit13@gmail.com>
2020-10-22 10:48:37 +0530

    modified

---
+++ BehaviouralPattern/com/pattern/template_method/Test.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.pattern.template_method;
+
+/**
+ * @author Rohit
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		AsusComputers computers = new LaptopManufacturingFacility();
+		computers.buidComputer();
+	}
+}
